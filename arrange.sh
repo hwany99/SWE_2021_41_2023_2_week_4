f1be69d5da70b6e5bb2790a4d2577bee8dfbf610
hwany99 <105199942+hwany99@users.noreply.github.com>
2023-09-20 19:52:39 +0900

    Update arrange.sh

--- arrange.sh
+++ arrange.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-
 for file in *; do
+    if [ "$file" = "arrange.sh" ]; then
+        continue
+    fi
     if [ -f "$file" ]; then
         first_letter=${file:0:1}
         lower_first_letter=$(echo $first_letter | tr '[:upper:]' '[:lower:]')
-        if [ ! -d "$lower_first_letter" ]; then
-            mkdir "$lower_first_letter"
+        
+        if [ ! -d "../$lower_first_letter" ]; then
+            mkdir "../$lower_first_letter"
         fi
-        mv "$file" "$lower_first_letter/"
+        
+        mv "$file" "../$lower_first_letter/"
     fi
 done
