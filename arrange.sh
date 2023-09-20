99539656e4f3c89df57c108aba7c5130606e7c6b
hwany99 <105199942+hwany99@users.noreply.github.com>
2023-09-20 19:18:00 +0900

    Add files via upload
    upload assignment4 
    -202315708_kimtaehwan-

---
+++ arrange.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+for file in *; do
+    if [ -f "$file" ]; then
+        first_letter=${file:0:1}
+        lower_first_letter=$(echo $first_letter | tr '[:upper:]' '[:lower:]')
+        if [ ! -d "$lower_first_letter" ]; then
+            mkdir "$lower_first_letter"
+        fi
+        mv "$file" "$lower_first_letter/"
+    fi
+done
